25cef14dd056924cb0a3eebd0455741b6a7b81d2
samuelfreeman <olatunjishinasamuel@gmail.com>
2016-11-04 12:43:44 +0100

    this is the duplicate

---
+++ Hello-world-local/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello_world_local.Startup))]
+namespace Hello_world_local
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
